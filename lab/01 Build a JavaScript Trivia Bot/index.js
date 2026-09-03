3411dff3471a263f0bca1c63a9c240e8d6165ed9
nugi-ML <nugisaputra212@gmail.com>
2026-09-03 10:40:01 +0700

    first commit

---
+++ lab/01 Build a JavaScript Trivia Bot/index.js
@@ -0,0 +1,22 @@
+console.log("Hello! I'm your coding fun fact guide!")
+
+const botName = "Rehan";
+const botLocation = "Bandung";
+const favoriteLanguage = "Python";
+
+const introBot = "My name is " + botName + " and I live on " + botLocation + ".";
+console.log(introBot)
+
+const langBot = "My favorite programming language is " + favoriteLanguage + ".";
+console.log(langBot)
+
+let codingFact = botName + " using " + favoriteLanguage + " and not use JavaScript."
+console.log(codingFact)
+
+codingFact = "I'm learn " + favoriteLanguage + " in freeCodeCamp."
+console.log(codingFact)
+
+codingFact = "and using " + favoriteLanguage + " for computational Physics."
+console.log(codingFact)
+
+console.log(`It was fun sharing these facts with you. Goodbye! - ${botName} from ${botLocation}.`);
---
+++ lab/02 Build a Sentence Maker/index.js
@@ -0,0 +1,19 @@
+let adjective = "brave";
+let noun = "dragon";
+let verb = "angry";
+let place = "cave";
+let adjective2 = "smoking";
+let noun2 = "tacos";
+
+let firstStory = `Once upon a time, there was a(n) ${adjective} ${noun} who loved to eat ${noun2}. The ${noun} lived in a ${place} and had ${adjective2} nostrils that blew fire when it was ${verb}.`;
+console.log(`First story: ${firstStory}`);
+
+adjective = "tiny";
+noun = "mouse";
+verb = "startled";
+place = "castle";
+adjective2 = "sparkling";
+noun2 = "cheese";
+
+let secondStory = `Once upon a time, there was a(n) ${adjective} ${noun} who loved to eat ${noun2}. The ${noun} lived in a ${place} and had ${adjective2} nostrils that blew fire when it was ${verb}.`;
+console.log(`Second story: ${secondStory}`);
